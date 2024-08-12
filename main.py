596ca1da703e8cfd8f9b03d7b0a675a2d9c162eb
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-12 19:49:44 +0545

    feat: Add functionality to insert node in a tree

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ class TreeNode:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+
+    def _insert(self, node, key):
+        if node is None:
+            return TreeNode(key)
+
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+
+            node.right = self._insert(node.right, key)
+        return node
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
