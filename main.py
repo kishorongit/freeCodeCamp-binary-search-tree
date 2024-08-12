81765febd098e1469009f0d3972252f5dd9570f6
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-12 17:41:46 +0545

    chore: Initialize project

---
+++ main.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
