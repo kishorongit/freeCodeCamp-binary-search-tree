4b4117bd9ea4992ecd6479bb03a125ab3d742b54
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-13 11:29:14 +0545

    feat: Add functionality to delete a node

--- main.py
+++ main.py
@@ -35,3 +35,29 @@ class BinarySearchTree:
 
     def search(self, key):
         return self._search(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+
+            node.key = self._min_value(node.right)
+            node.right = self._delete(node.right, node.key)
+
+        return node
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _min_value(self, node):
+        while node.left is not None:
+            node = node.left
+        return node.key
